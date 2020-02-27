ea6d66ff3643b84dc3096e09f23bee4d7e73377c
Emperor577 <kholmuhammadov@gmail.com>
2020-02-27 16:51:50 +0500

    added widgets

---
+++ app/Widgets/Messages.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Widgets;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class Messages extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Models\Message::count();
+        $string = 'Сообщения';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-mail',
+            'title'  => "{$count} {$string}",
+            'text'   => 'У вас есть '. $count .' новых сообщения',
+            'button' => [
+                'text' => 'Все сообщения',
+                'link' => route('voyager.users.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/01.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('User'));
+    }
+}
